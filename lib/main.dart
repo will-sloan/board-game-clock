d5b342c6dbfaaed51c2e65a41bcc596ed0522caa
Will Sloan <willis5552@gmail.com>
2020-07-10 12:01:17 -0400

    going

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,192 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(TimerApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class TimerApp extends StatelessWidget {
+  var a = 5;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "Timer App",
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TimerBody(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class TimerBody extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _TimerBodyState createState() => _TimerBodyState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TimerBodyState extends State<TimerBody> {
+  var players = 0;
+  var duration = 0;
+  final _formKey = GlobalKey<FormState>();
+  final playerController = TextEditingController();
+  final durationController = TextEditingController();
 
-  void _incrementCounter() {
+  List colorList = [
+    Colors.red,
+    Colors.blue,
+    Colors.green,
+    Colors.orange,
+    Colors.purple,
+    Colors.brown,
+    Colors.yellow,
+    Colors.indigo,
+    Colors.lime,
+    Colors.teal,
+    Colors.cyan,
+    Colors.pink
+  ];
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    durationController.dispose();
+    playerController.dispose();
+    super.dispose();
+  }
+
+  void _setPlayers(num_players) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      players = num_players;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        title: Text("Timer App"),
+        leading: IconButton(
+          icon: Icon(Icons.ac_unit),
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    content: Stack(
+                      overflow: Overflow.visible,
+                      children: <Widget>[
+                        Positioned(
+                          right: -40.0,
+                          top: -40.0,
+                          child: InkResponse(
+                            onTap: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: CircleAvatar(
+                              child: Icon(Icons.close),
+                              backgroundColor: Colors.red,
+                            ),
+                          ),
+                        ),
+                        Form(
+                          key: _formKey,
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              // Padding(
+                              //   padding: EdgeInsets.all(8.0),
+                              //   child: TextFormField(),
+                              // ),
+                              Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: TextFormField(
+                                  controller: playerController,
+                                  decoration: const InputDecoration(
+                                    hintText: "Number of Players",
+                                  ),
+                                  validator: (value) {
+                                    if (int.tryParse(value) == null) {
+                                      return "Must input a number between 1-12";
+                                    } else if (int.tryParse(value) < 1 ||
+                                        int.tryParse(value) > 12) {
+                                      return "Number must be between 1-12";
+                                    } else {
+                                      return null;
+                                    }
+                                  },
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: TextFormField(
+                                  controller: durationController,
+                                  decoration: const InputDecoration(
+                                    hintText: "Duration (minutes)",
+                                  ),
+                                  validator: (value) =>
+                                      int.tryParse(value) == null
+                                          ? "Must be a number"
+                                          : null,
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: RaisedButton(
+                                  child: Text("Submit"),
+                                  onPressed: () {
+                                    if (_formKey.currentState.validate()) {
+                                      setState(() {
+                                        players =
+                                            int.parse(playerController.text);
+                                        duration =
+                                            int.parse(durationController.text);
+                                      });
+                                      Navigator.of(context).pop();
+                                    }
+                                  },
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                });
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Column(
+          children: List.generate(
+              players,
+              (index) => Expanded(
+                  child: Container(
+                      // height:
+                      //     MediaQuery.of(context).size.height /
+                      //         players,
+                      color: colorList[index],
+                      child: Text("$index"))))),
+      // body: MediaQuery.of(context).orientation == Orientation.portrait
+      //     ? portraitMode(context, )
+      //     : landscapeMode(),
+      //   OrientationBuilder(builder: (context, orientation) {
+      //     return orientation == Orientation.portrait
+      //         ? portraitMode()
+      //         : landscapeMode();
+      //   }
+      // )
     );
   }
 }
+
+// Widget portraitMode(context) {
+//   var height = MediaQuery.of(context).size.height;
+//   var width = MediaQuery.of(context).size.width;
+//   return Column(children: <Widget>[
+//     Container(height: height, width: width, child: Text(""))
+//   ],)
+// }
+
+// Widget landscapeMode() {
+//   return Container(child: Text("Bye"));
+// }
